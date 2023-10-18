f09c72f1f3450701baa26e593c686c9065ab6ce9
Robert Mang <Robert.Mang@softgarden.de>
2023-10-19 01:57:12 +0200

    fix movement when pressing more than one direction at once

--- player/player.cs
+++ player/player.cs
@@ -6,8 +6,6 @@ public partial class player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	// public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	public AnimationPlayer PlayerAnimation;
 	public AnimatedSprite2D PlayerSprite;
 
@@ -20,16 +18,6 @@ public partial class player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		// if (!IsOnFloor())
-		//	velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		//	velocity.Y = JumpVelocity;
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		
 		if (direction != Vector2.Zero)
@@ -44,16 +32,19 @@ public partial class player : CharacterBody2D
 
 			if(direction == Vector2.Up){
 				PlayerAnimation.Play("run_up");
+				velocity.X = 0;
 				velocity.Y = direction.Y * Speed;
 			}
 
 			if(direction == Vector2.Down){
 				PlayerAnimation.Play("run_down");
+				velocity.X = 0;
 				velocity.Y = direction.Y * Speed;
 			}
 
 			if(direction == Vector2.Left || direction == Vector2.Right){
 				PlayerAnimation.Play("run_side");
+				velocity.Y = 0;
 				velocity.X = direction.X * Speed;
 			}
 		}
@@ -66,10 +57,6 @@ public partial class player : CharacterBody2D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
-
-		//if (velocity.Y > 0) {
-		//	PlayerAnimation.Play("fall");
-		//}
 
 		Velocity = velocity;
 		MoveAndSlide();

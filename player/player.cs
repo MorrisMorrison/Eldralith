107a007957a96313e24848c34aec26bae6861643
Robert Mang <Robert.Mang@softgarden.de>
2023-10-29 08:33:55 +0100

    setup multiplayer camera

--- player/player.cs
+++ player/player.cs
@@ -3,32 +3,76 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 200.0f;
-	public const float JumpVelocity = -400.0f;
-	private Vector2 syncPos = new Vector2(0, 0);
+	private const float _speed = 200.0f;
+	private const float _jumpVelocity = -400.0f;
+	private Vector2 _syncPos = new(0, 0);
+	private float _cameraFollowSpeed = 10f;
 
-	public AnimationPlayer PlayerAnimation;
-	public AnimatedSprite2D PlayerSprite;
+	private AnimationPlayer _playerAnimation;
+	private AnimatedSprite2D _playerSprite;
+	private Camera2D _playerCamera;
+	private MultiplayerSynchronizer _multiplayerSynchronizer;
+
 
 	public override void _Ready()
 	{
-		PlayerAnimation = GetNode<AnimationPlayer>("AnimationPlayer");
-		PlayerSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_playerAnimation = GetNode<AnimationPlayer>("AnimationPlayer");
+		_playerSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer").SetMultiplayerAuthority(int.Parse(Name));
+		_multiplayerSynchronizer = GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer");
+		_multiplayerSynchronizer.SetMultiplayerAuthority(int.Parse(Name));
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer").GetMultiplayerAuthority() == Multiplayer.GetUniqueId())
+		if (IsCurrentPlayer())
 		{
 			HandleCurrentPlayerMovement();
-			syncPos = GlobalPosition;
+			_syncPos = GlobalPosition;
 		}
 		else
 		{
-			GlobalPosition = GlobalPosition.Lerp(syncPos, .1f);
+			GlobalPosition = GlobalPosition.Lerp(_syncPos, .1f);
 		}
+	}
+
+	private bool IsCurrentPlayer()
+	{
+		return _multiplayerSynchronizer.GetMultiplayerAuthority() == Multiplayer.GetUniqueId();
+	}
+
+	private void SetupPlayerCamera()
+	{
+		if (!IsCurrentPlayer()) return;
+
+		Camera2D playerCamera = new Camera2D
+		{
+			Enabled = true,
+			Name = "PlayerCamera",
+			AnchorMode = Camera2D.AnchorModeEnum.DragCenter,
+			Zoom = new Vector2(1.6f, 1.6f),
+			Position = Position,
+			LimitLeft = 0,
+			LimitTop = 0,
+			LimitRight = (int)GetViewportRect().Size.X,
+			LimitBottom = (int)GetViewportRect().Size.Y
+		};
+
+		playerCamera.MakeCurrent();
+		_playerCamera = playerCamera;
+
+		AddChild(_playerCamera);
+	}
+
+	private void HandlePlayerCamera(double delta)
+	{
+		Vector2 cameraPosition = _playerCamera.Position;
+		Vector2 targetPosition = Position - GetViewportRect().Size / 2;
+		// Calculate the new camera position based on the player's position
+		cameraPosition = cameraPosition.Lerp(targetPosition, (float)delta * _cameraFollowSpeed);
+
+		// Set the new camera position
+		_playerCamera.Position = cameraPosition;
 	}
 
 	private void HandleCurrentPlayerMovement()
@@ -40,44 +84,44 @@ public partial class Player : CharacterBody2D
 		{
 			if (direction == Vector2.Left)
 			{
-				PlayerSprite.FlipH = true;
+				_playerSprite.FlipH = true;
 			}
 
 			if (direction == Vector2.Right)
 			{
-				PlayerSprite.FlipH = false;
+				_playerSprite.FlipH = false;
 			}
 
 			if (direction == Vector2.Up)
 			{
-				PlayerAnimation.Play("run_up");
+				_playerAnimation.Play("run_up");
 				velocity.X = 0;
-				velocity.Y = direction.Y * Speed;
+				velocity.Y = direction.Y * _speed;
 			}
 
 			if (direction == Vector2.Down)
 			{
-				PlayerAnimation.Play("run_down");
+				_playerAnimation.Play("run_down");
 				velocity.X = 0;
-				velocity.Y = direction.Y * Speed;
+				velocity.Y = direction.Y * _speed;
 			}
 
 			if (direction == Vector2.Left || direction == Vector2.Right)
 			{
-				PlayerAnimation.Play("run_side");
+				_playerAnimation.Play("run_side");
 				velocity.Y = 0;
-				velocity.X = direction.X * Speed;
+				velocity.X = direction.X * _speed;
 			}
 		}
 		else
 		{
 			if (velocity.Y == 0 && velocity.X == 0)
 			{
-				PlayerAnimation.Play("idle_down");
+				_playerAnimation.Play("idle_down");
 			}
 
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, _speed);
 		}
 
 		Velocity = velocity;

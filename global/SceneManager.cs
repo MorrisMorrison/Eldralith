bb522df1e7edc6603a6caf3c5e398637c01d64e9
Robert Mang <Robert.Mang@softgarden.de>
2023-10-29 09:18:49 +0100

    use functional style for each loops

--- global/SceneManager.cs
+++ global/SceneManager.cs
@@ -5,28 +5,21 @@ public partial class SceneManager : Node
 {
 
 	[Export]
-	private PackedScene playerScene;
+	private PackedScene _playerScene;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		int index = 0;
-		foreach (PlayerInfo player in GameManager.Players)
-		{
-			Player currentPlayer = playerScene.Instantiate<Player>();
+		GameManager.Players.Each((player, index) => {
+			Player currentPlayer = _playerScene.Instantiate<Player>();
 			currentPlayer.Name = player.Id.ToString();
 			AddChild(currentPlayer);
 
-			foreach (Node2D playerSpawnPoint in GetTree().GetNodesInGroup("PlayerSpawnPoints"))
-			{
-				GD.Print("PlayerSpawnPoint");
-				if (int.Parse(playerSpawnPoint.Name) == index)
-				{
-					GD.Print("PlayerSpawnPoint -> set global pos");
-					currentPlayer.GlobalPosition = playerSpawnPoint.GlobalPosition;
-				}
-			}
-			index++;
-		}
+			GetTree().GetNodesInGroup("PlayerSpawnPoints").Each(playerSpawnPoint => {
+				if (int.Parse(playerSpawnPoint.Name) != index) return;
+					
+				currentPlayer.GlobalPosition = ((Node2D) playerSpawnPoint).GlobalPosition;
+			});
+		});
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
